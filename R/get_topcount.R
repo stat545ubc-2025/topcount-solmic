5d1f3b5a42aca1c30198e1f6190ac7f2915530f5
solmic <Smichaud@sandiego.edu>
2025-11-19 11:28:32 -0800

    Initial commit

--- R/get_topcount.R
+++ R/get_topcount.R
@@ -1,6 +1,7 @@
 #'@title Top count of a data set
 #'@description
 #'This function will output the top count for a data set.
+#'If dplyr isn't imported ensure all functions from external packages use package::function()
 #'
 #'@param data A data frame (tibble or data.frame).
 #'@param col An unquoted column name (tidyselect). The column to count by.
@@ -9,8 +10,9 @@
 #'@return A tibble with two columns: the grouping column and `count`.
 #'
 #'@examples
-#'df <- data.frame(city = c("A","A","B","C","A","B"))
-#'get_topcount(df, city, n = 2)
+#'library(tibble)
+#'df <- tibble(city = c("A","A","B","C","A","B"))
+#'topcount::get_topcount(df, city, n = 2)
 #'@import dplyr
 #'@export
 
--- tests/testthat/test-get_topcount.R
+++ tests/testthat/test-get_topcount.R
@@ -3,11 +3,11 @@ df <- data.frame(group = c("A", "B", "A", "C", "A", "B"))
 test_that("topcounts works correctly", {
 
   # Expect error when n is not numeric
-  expect_error(topcount(df, group, n = "two"))
+  expect_error(get_topcount(df, group, n = "two"))
 
   # Expect error when n is negative
-  expect_error(topcount(df, group, n = -5))
+  expect_error(get_topcount(df, group, n = -5))
 
   # Expect correct number of returned rows
-  expect_equal(nrow(top_count(df, group, n = 2)), 2)
+  expect_equal(nrow(get_topcount(df, group, n = 2)), 2)
 })
